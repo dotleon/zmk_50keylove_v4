26699d02dd2746c08829d0985c03df56838c39b9
dotleon <34742339+dotleon@users.noreply.github.com>
2025-10-07 00:43:26 +0200

    forgot to shift widgets 28 pixel after removing the shift from inside of them

--- boards/shields/minidongle_display/custom_status_screen.c
+++ boards/shields/minidongle_display/custom_status_screen.c
@@ -39,7 +39,7 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
     
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 28, 0);
     
     zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
     //lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_RIGHT, 0, -7);
@@ -64,7 +64,7 @@ lv_obj_t *zmk_display_status_screen() {
     //lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
 
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
-    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_BOTTOM_LEFT, 28, 0);
 
     return screen;
 }
